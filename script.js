4f310fc88d815f43b1af313a345a7ecce5853751
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 13:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-//your code here
-
-document.addEventListener("DOMContentLoaded", function () {
-  const bandNames = [
-    'The Virupaksha Temple',
-    'Victoria Memorial',
-    'Tajmahal'
-  ];
-
-  // Function to remove articles and return the cleaned name
-  const removeArticles = (bandName) => {
-    return bandName.replace(/^(a |an |the )/i, '').trim();
-  };
-
-  // Sort the band names in lexicographic order
-  const sortedBandNames = bandNames.slice().sort((a, b) => {
-    const nameA = removeArticles(a).toUpperCase();
-    const nameB = removeArticles(b).toUpperCase();
-    return nameA.localeCompare(nameB);
-  });
-
-  // Get the ul element by id
-  const ulElement = document.getElementById('bands');
-
-  // Create and append li elements for each band name
-  sortedBandNames.forEach((bandName) => {
-    const liElement = document.createElement('li');
-    liElement.textContent = bandName;
-    ulElement.appendChild(liElement);
-  });
-});
+let arr = ['Tajmahal', 'The Virupaksha Temple', 'Victoria Memorial'];
+function getArticleLessString(input) {
+    // example: input: "The tajmahal an umberilla" , output: "tajmahal umberiall"
+    // words = "The tajmahal an umberilla".split(" ") = [ "The", "tajmahal", "an", "umberilla" ] 
+    // nonArticleWords = [ "tajmahal", "umberilla"].join(" ") => "tajmahal umberilla"
+    let words = input.split(" ");
+    let nonArticleWords = words.filter(
+        // words = [ "The", "tajmahal", "an", "umberilla" ] 
+        (word) => {
+            let smallCaseWord = word.toLowerCase();
+            if(smallCaseWord === "the" || smallCaseWord === "an" || smallCaseWord === "a"){
+                return false;
+            }
+            return true ;
+        }
+    );
