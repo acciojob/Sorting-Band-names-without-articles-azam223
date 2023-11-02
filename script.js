525458be3091c87b1499a4eff8cd725972286a17
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 14:08:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-Let bandNames = ['The Rolling Stone', "Led Zeppelin', 'Aerosmith', "The Beatles',
-/ Function to remove articles and return the modified band name function
-removeArticles (name) {
+Let bandNames = ['The Rolling Stone', "Led Zeppelin', 'Aerosmith', 'The Beatles']
+// Function to remove articles and return the modified band name function
+function removeArticles (name) {
 return name.replace(/(a Jan |the )/f, ).trim);
 }
-11 Remove articles from all band names
-let modifiedBandNames = bandNames. map(removeArticles);
-I Sort the modified band names lexicographically modifiedBandNames. sort();
-It Create an ul element with id band"
+let modifiedBandNames = bandNames.map(removeArticles);
 let ulElement = document. createElement (ul);
-ulElement. id = "band';
-Il Add li elements to the ul element
-modifiedBandNames. for Each (function (bandName) f
-Let LiElement - document. createElement(li);
+ulElement.id = "band';
+modifiedBandNames.for Each (function (bandName) f
+let liElement = document.createElement('li');
 liElement. textContent = bandName;
 ulElement. appendchild(liElement);
 });
