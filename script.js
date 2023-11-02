50cf7fabd41dc3feabcfe5e0b1c3f5e681b73ec9
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 13:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
 
+document.addEventListener("DOMContentLoaded", function () {
+  const bandNames = [
+    'The Virupaksha Temple',
+    'Victoria Memorial',
+    'Tajmahal'
+  ];
+
+  // Function to remove articles and return the cleaned name
+  const removeArticles = (bandName) => {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
+  };
+
+  // Sort the band names in lexicographic order
+  const sortedBandNames = bandNames.slice().sort((a, b) => {
+    const nameA = removeArticles(a).toUpperCase();
+    const nameB = removeArticles(b).toUpperCase();
+    return nameA.localeCompare(nameB);
+  });
+
+  // Get the ul element by id
+  const ulElement = document.getElementById('bands');
+
+  // Create and append li elements for each band name
+  sortedBandNames.forEach((bandName) => {
+    const liElement = document.createElement('li');
+    liElement.textContent = bandName;
+    ulElement.appendChild(liElement);
+  });
+});
