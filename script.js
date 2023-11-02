e24627e44fcbdbd2dc2c6610f2f193599b9ef536
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 13:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-let arr = ['Tajmahal', 'The Virupaksha Temple', 'Victoria Memorial'];
-function getArticleLessString(input) {
-    // example: input: "The tajmahal an umberilla" , output: "tajmahal umberiall"
-    // words = "The tajmahal an umberilla".split(" ") = [ "The", "tajmahal", "an", "umberilla" ] 
-    // nonArticleWords = [ "tajmahal", "umberilla"].join(" ") => "tajmahal umberilla"
-    let words = input.split(" ");
-    let nonArticleWords = words.filter(
-        // words = [ "The", "tajmahal", "an", "umberilla" ] 
-        (word) => {
-            let smallCaseWord = word.toLowerCase();
-            if(smallCaseWord === "the" || smallCaseWord === "an" || smallCaseWord === "a"){
-                return false;
-            }
-            return true ;
-        }
-    );
-    // nonArticleWords = [ "tajmahal", "umberilla" ]
-    return nonArticleWords.join(" "); //  "tajmahal umberilla"
+let result={};
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let articlelessarray=[];
+let articlelessitem=[];
+for(let i=0;i<touristSpots.length;i++){
+	articlelessitem= touristSpots[i].replace(/\bthe\b | \ban\b | \ba\b /gi," ").trim();
+	articlelessarray.push(articlelessitem);
+	result[articlelessitem]=touristSpots[i];
 }
-let hashMap = {} ;
-let arr1 = [] ;
-for(let i = 0; i < arr.length; i++) {
-    let articleLessString = getArticleLessString(arr[i]);
-    hashMap[articleLessString] = arr[i];
-    arr1.push(articleLessString);
+articlelessarray.sort();
+let ans=[];
+for(let i of articlelessarray){
+	ans.push(result[i]);
 }
-// arr1 = [ 'Tajmahal', 'Virupaksha Temple' , 'Victoria Memorial']
-arr1.sort();
-// arr1 = [ 'Tajmahal', 'Victoria Memorial' , 'Virupaksha Temple']
-let outputList = [] ;
-// [ "Tajmahal", "Victoria Memorial", 'The Virupaksha Temple']
-arr1.forEach((newString) => {
-    outputList.push( hashMap[newString] ) ; // 
-})
-console.log(outputList);
+console.log(ans);
