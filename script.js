27fb942e159db35bfc5eef587c453cce94fab5bf
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 13:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let arr = ['Tajmahal', 'The Virupaksha Temple', 'Victoria Memorial'];
+function getArticleLessString(input) {
+    // example: input: "The tajmahal an umberilla" , output: "tajmahal umberiall"
+    // words = "The tajmahal an umberilla".split(" ") = [ "The", "tajmahal", "an", "umberilla" ] 
+    // nonArticleWords = [ "tajmahal", "umberilla"].join(" ") => "tajmahal umberilla"
+    let words = input.split(" ");
+    let nonArticleWords = words.filter(
+        // words = [ "The", "tajmahal", "an", "umberilla" ] 
+        (word) => {
+            let smallCaseWord = word.toLowerCase();
+            if(smallCaseWord === "the" || smallCaseWord === "an" || smallCaseWord === "a"){
+                return false;
+            }
+            return true ;
+        }
+    );
+    // nonArticleWords = [ "tajmahal", "umberilla" ]
+    return nonArticleWords.join(" "); //  "tajmahal umberilla"
+}
 let hashMap = {} ;
 let arr1 = [] ;
 for(let i = 0; i < arr.length; i++) {
