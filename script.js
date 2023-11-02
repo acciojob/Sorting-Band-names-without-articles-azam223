686be9753fee4df602304d118e31f0c95b41eda7
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-02 14:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let result={};
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let articlelessarray=[];
-let articlelessitem=[];
-for(let i=0;i<touristSpots.length;i++){
-	articlelessitem= touristSpots[i].replace(/\bthe\b | \ban\b | \ba\b /gi," ").trim();
-	articlelessarray.push(articlelessitem);
-	result[articlelessitem]=touristSpots[i];
+Let bandNames = ['The Rolling Stone', "Led Zeppelin', 'Aerosmith', "The Beatles',
+/ Function to remove articles and return the modified band name function
+removeArticles (name) {
+return name.replace(/(a Jan |the )/f, ).trim);
 }
-articlelessarray.sort();
-let ans=[];
-for(let i of articlelessarray){
-	ans.push(result[i]);
-}
-console.log(ans);
+11 Remove articles from all band names
+let modifiedBandNames = bandNames. map(removeArticles);
+I Sort the modified band names lexicographically modifiedBandNames. sort();
+It Create an ul element with id band"
+let ulElement = document. createElement (ul);
+ulElement. id = "band';
+Il Add li elements to the ul element
+modifiedBandNames. for Each (function (bandName) f
+Let LiElement - document. createElement(li);
+liElement. textContent = bandName;
+ulElement. appendchild(liElement);
+});
+ document.body•appendChild(ulElement);
